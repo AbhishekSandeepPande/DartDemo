d13548ce2e7fae03425e9eaa5bcfe64fdcfcd440
AbhishekSandeepPande <156991146+AbhishekSandeepPande@users.noreply.github.com>
2024-01-24 19:52:48 +0530

    Add files via upload

---
+++ quiz/quiz3_CondiStats1/quiz4.dart
@@ -0,0 +1,11 @@
+void main() {
+  String ch="a";
+  if(ch=="a" || ch=="e" || ch=="i" || ch=="o" || ch=="o"){
+    print("ch is vowel");
+  }
+  else{
+    print("ch is a consonant");
+  }
+}
+
+//ch is vowel
---
+++ quiz/quiz3_CondiStats1/quiz9.dart
@@ -0,0 +1,18 @@
+void main(){
+  int x=100;
+  int y=200;
+
+  if(y%x < 0 && (++x + 100>y)){
+    print("x");
+  }
+  else if(x~/y !=0){
+    print("y");
+  }
+  else{
+    print("xy");
+  }
+  print(x);
+}
+
+//inside else block
+// xy 100
